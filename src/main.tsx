f87ec0548982a727e5770d85e6f6882f57d51224
Yin-Te Lan <k0960355356@gmail.com>
2024-01-30 00:25:15 +0800

    <feat><mui>

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App';
 import { AuthContextProvider } from './context/authContext';
 import { DarkModeContextProvider } from './context/darkModeContext';
 import { WebSocketContextProvider } from './context/webSocketContext';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
