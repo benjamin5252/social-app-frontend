44ce7f3bd84964a6780114544b322999686f8b07
benjamin5252 <david.l@amaryllo.us>
2024-01-30 16:46:31 +0800

    <feat><dotenv>

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Route,
   Outlet,
   Navigate,
+  BrowserRouter,
 } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import LeftBar from './components/leftBar/LeftBar';
@@ -18,6 +19,7 @@ import { DarkModeContext } from './context/darkModeContext';
 import { AuthContext } from './context/authContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.scss';
+import { basename } from 'path';
 
 const queryClient = new QueryClient();
 
@@ -51,34 +53,37 @@ function App() {
     return children;
   };
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: '/',
-          element: <Home />,
-        },
-        {
-          path: '/profile/:id',
-          element: <Profile />,
-        },
-      ],
-    },
-    {
-      path: '/login',
-      element: <Login />,
-    },
-    {
-      path: '/register',
-      element: <Register />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: (
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        ),
+        children: [
+          {
+            path: '/',
+            element: <Home />,
+          },
+          {
+            path: '/profile/:id',
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
+    { basename: process.env.BASE },
+  );
 
   return (
     <div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
+    server: {
+      port: 3000,
+    },
+    plugins: [react()],
+    base: process.env.BASE,
+    define: {
+      'process.env': process.env,
+    },
+  });
+};
