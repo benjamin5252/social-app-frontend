b17a70a4baca0dee11dbe87bd6f92c36e01198f1
benjamin5252 <david.l@amaryllo.us>
2024-01-31 09:34:23 +0800

    <feat><axios client>

--- src/axios.js
+++ src/axios.js
@@ -14,7 +14,7 @@ makeRequest.interceptors.response.use(
     const status = error.response?.status || 500;
     if (status === 401 || status === 403) {
       localStorage.removeItem('user');
-      setTimeout(() => (location.href = '/login'), 1000);
+      setTimeout(() => (location.href = '/#/login'), 1000);
     } else {
       return Promise.reject(error); // Delegate error to calling side
     }
