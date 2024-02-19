0b78b032524c4f2135add412896af2ec2b4a4bca
benjamin5252 <david.l@amaryllo.us>
2024-02-19 10:54:17 +0800

    <feat><resize photo>

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,12 +1,12 @@
 import './share.scss';
-import Image from '../../assets/img.png';
+import ImageIcon from '../../assets/img.png';
 import Map from '../../assets/map.png';
 import Friend from '../../assets/friend.png';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import makeRequest from '../../axios';
-import DefaultProfile from '../../assets/user_profile.jpg'
+import DefaultProfile from '../../assets/user_profile.jpg';
 
 const Share = () => {
   const [file, setFile] = useState(null);
@@ -36,10 +36,55 @@ const Share = () => {
     mutation.mutate({ desc, img: imgUrl });
   };
 
+  const resizeImageFile = (blob) => {
+    console.log('resize');
+    let blobUrl = URL.createObjectURL(blob);
+
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = (err) => reject(err);
+      img.src = blobUrl;
+    }).then((img) => {
+      URL.revokeObjectURL(blobUrl);
+      // Limit to 256x256px while preserving aspect ratio
+      let [w, h] = [img.width, img.height];
+      let aspectRatio = w / h;
+      // Say the file is 1920x1080
+      // divide max(w,h) by 256 to get factor
+
+      w = 320;
+
+      h = w / aspectRatio;
+
+      if (h > window.innerHeight * 0.6) {
+        h = window.innerHeight * 0.6;
+        w = h * aspectRatio;
+      }
+
+      // REMINDER
+      // 256x256 = 65536 pixels with 4 channels (RGBA) = 262144 data points for each image
+      // Data is encoded as Uint8ClampedArray with BYTES_PER_ELEMENT = 1
+      // So each images = 262144bytes
+      // 1000 images = 260Mb
+
+      let canvas = document.createElement('canvas');
+      canvas.width = w;
+      canvas.height = h;
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, w, h);
+
+      return new Promise((resolve) => {
+        canvas.toBlob(resolve); // implied image/png format
+      });
+    });
+  };
+
   const upload = async () => {
     try {
       const formData = new FormData();
-      formData.append('file', file);
+      const resizedFile = await resizeImageFile(file);
+      formData.append('file', resizedFile);
       const res = await makeRequest.post('/upload', formData);
       return res.data;
     } catch (err) {
@@ -53,7 +98,11 @@ const Share = () => {
         <div className="top">
           <div className="left">
             <img
-              src={ currentUser.profilePic ? 'http://localhost:8000/upload/' + currentUser.profilePic : DefaultProfile}
+              src={
+                currentUser.profilePic
+                  ? 'http://localhost:8000/upload/' + currentUser.profilePic
+                  : DefaultProfile
+              }
               alt="DefaultProfile"
             />
             <input
@@ -80,7 +129,7 @@ const Share = () => {
             />
             <label htmlFor="file">
               <div className="item">
-                <img src={Image} alt="" />
+                <img src={ImageIcon} alt="" />
                 <span>Add Image</span>
               </div>
             </label>
